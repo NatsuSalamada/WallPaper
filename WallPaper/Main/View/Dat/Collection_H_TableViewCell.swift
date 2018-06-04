1d4bd7c755c1b749d55584c4f218e7d27fdb8365
Unknown <natsusalamada@gmail.com>
2018-06-04 17:43:34 +0700

    Fix tableView Cate

---
+++ WallPaper/Main/View/Dat/Collection_H_TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Collection_H_TableViewCell.swift
+//  WallPaper
+//
+//  Created by Bé Nhện Của Bé Thảo on 04/06/2018.
+//  Copyright © 2018 NatsuSalamada. All rights reserved.
+//
+
+import UIKit
+
+class Collection_H_TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
