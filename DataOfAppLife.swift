86a5151b461067e0c2af57bdf21492595930de42
Unknown <natsusalamada@gmail.com>
2018-06-04 11:26:09 +0700

    update Fonts

---
+++ DataOfAppLife.swift
@@ -0,0 +1,30 @@
+//
+//  DataOfAppLife.swift
+//  FaceDetection
+//
+//  Created by MacBook on 8/31/17.
+//  Copyright © 2017 com.Natsu.ChatStories. All rights reserved.
+//
+// Lam Download
+//// Down Cate
+//// Down ListImage
+//// setup version download new image
+import Foundation
+import StoreKit
+import AVFoundation
+
+var Banner_ID:String = "ca-app-pub-3940256099942544/2934735716"
+var Interstitial_ID:String = "ca-app-pub-3940256099942544/4411468910"
+var linkApp:String = "https://itunes.apple.com/app/viewContentsUserReviews?id=1370297772"  // link rate
+var linkDownload:String = "https://itunes.apple.com/app/viewContentsUserReviews?id=1370297772"  // link dowload game
+var RewardVideo_ID:String = "ca-app-pub-3940256099942544/1712485313"
+var mail:String = "mr.tuananh8@gmail.com"
+var urlAppGuide:URL = URL(string: linkApp)!
+var url:URL = URL(string: linkDownload)!
+var timeLitmit = 120
+var timeAdShow = 0
+
+
+
+
+
