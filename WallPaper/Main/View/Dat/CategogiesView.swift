bf7b91c14f586b1dad1a4f840ce6387bb2b0445b
Unknown <natsusalamada@gmail.com>
2018-06-05 14:40:40 +0700

    fix Collection

--- WallPaper/Main/View/Dat/CategogiesView.swift
+++ WallPaper/Main/View/Dat/CategogiesView.swift
@@ -8,9 +8,16 @@
 
 import UIKit
 
+struct CellCollectionCate {
+    static let Cell_H = "Coll_H"
+    static let Cell_V = "Coll_V"
+}
+
 class CategogiesView: UIViewController ,UITableViewDelegate,UITableViewDataSource,UICollectionViewDataSource,UICollectionViewDelegate {
     
     var img = ["hinh1","hinh2","hinh3","hinh4","hinh5"]
+    
+    var identifier = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +32,41 @@ class CategogiesView: UIViewController ,UITableViewDelegate,UITableViewDataSourc
         return 6
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = UITableViewCell()
+        
+       
         
         if indexPath.row == 0 || indexPath.row == 2 || indexPath.row == 4{
-            cell = tableView.dequeueReusableCell(withIdentifier: "CellLbl", for: indexPath) as! Name_Cate_TableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CellLbl", for: indexPath) as! Name_Cate_TableViewCell
+            return cell
            
         }else if indexPath.row == 1 || indexPath.row == 3{
-            cell = tableView.dequeueReusableCell(withIdentifier: "Cell_H", for: indexPath) as! Collection_H_TableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell_H", for: indexPath) as! Collection_H_TableViewCell
+            self.identifier = CellCollectionCate.Cell_H
+            return cell
         }else{
-            cell = tableView.dequeueReusableCell(withIdentifier: "Cell_V", for: indexPath) as! Collection_V_TableViewCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell_V", for: indexPath) as! Collection_V_TableViewCell
+            self.identifier = CellCollectionCate.Cell_V
+            return cell
         }
         
-        return cell
+        
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return img.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Coll_H", for: indexPath) as! Horizontal_CollectionView
-        cell.H_Image.image = UIImage(named: img[indexPath.row])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        if identifier == CellCollectionCate.Cell_H{
+            if let Cell_H_Collection = cell as? Horizontal_CollectionView{
+                Cell_H_Collection.H_Image.image = UIImage(named: img[indexPath.row])
+            }
+        }else{
+            if let Cell_V_Collection = cell as? Vertical_CollectionView{
+                
+            }
+        }
+        
         return cell
     }
 
