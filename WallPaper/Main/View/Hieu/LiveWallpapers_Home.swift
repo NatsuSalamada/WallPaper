4ab7e3706787dfc67df42d37fc8b5012bbe78ee2
eciscbss <eciscbs@gmail.com>
2018-06-21 15:38:26 +0700

    FadedView+CollectionViewHome

--- WallPaper/Main/View/Hieu/LiveWallpapers_Home.swift
+++ WallPaper/Main/View/Hieu/LiveWallpapers_Home.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-class LiveWallpapers_Home: UIViewController {
+class LiveWallpapers_Home: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+   
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: CollView_LiveWallHome.bounds.width, height: CollView_LiveWallHome.bounds.height)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell_LiveWallpapersHome", for: indexPath) as! CollViewCell_Home
+        return cell
+    }
+    
+    
+    
 
+    @IBOutlet weak var CollView_LiveWallHome: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
