f74ab484306f2fb5b099b3ea2c496de8c3f09f73
Unknown <tl.dat318@gmail.com>
2018-06-21 08:48:28 +0700

    Livewallpaper Home Dat

--- WallPaper/Main/View/Hieu/LiveWallpapers_Home.swift
+++ WallPaper/Main/View/Hieu/LiveWallpapers_Home.swift
@@ -1,35 +1,91 @@
-//
-//  LiveWallpapers_Home.swift
-//  WallPaper
-//
-//  Created by 5k on 6/6/18.
-//  Copyright © 2018 NatsuSalamada. All rights reserved.
-//
-
 import UIKit
+import Photos
+import PhotosUI
+import MobileCoreServices
 
-class LiveWallpapers_Home: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+struct FilePaths {
+    static let documentsPath : AnyObject = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask,true)[0] as AnyObject
+    struct VidToLive {
+        static var livePath = FilePaths.documentsPath.appending("/")
+    }
+}
 
-        // Do any additional setup after loading the view.
+class LiveWallpapers_Home: UIViewController, UIImagePickerControllerDelegate {
+    @IBOutlet weak var livePhotoView: PHLivePhotoView! {
+        didSet {
+            loadVideoWithVideoURL(Bundle.main.url(forResource: "Spacedoggy", withExtension: "MOV")!)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func loadVideoWithVideoURL(_ videoURL: URL) {
+        livePhotoView.livePhoto = nil
+        let asset = AVURLAsset(url: videoURL)
+        let generator = AVAssetImageGenerator(asset: asset)
+        generator.appliesPreferredTrackTransform = true
+        let time = NSValue(time: CMTimeMakeWithSeconds(CMTimeGetSeconds(asset.duration)/2, asset.duration.timescale))
+        generator.generateCGImagesAsynchronously(forTimes: [time]) { [weak self] _, image, _, _, _ in
+            if let image = image, let data = UIImagePNGRepresentation(UIImage(cgImage: image)) {
+                let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+                let imageURL = urls[0].appendingPathComponent("image.jpg")
+                try? data.write(to: imageURL, options: [.atomic])
+                
+                let image = imageURL.path
+                let mov = videoURL.path
+                let output = FilePaths.VidToLive.livePath
+                let assetIdentifier = UUID().uuidString
+                let _ = try? FileManager.default.createDirectory(atPath: output, withIntermediateDirectories: true, attributes: nil)
+                do {
+                    try FileManager.default.removeItem(atPath: output + "/IMG.JPG")
+                    try FileManager.default.removeItem(atPath: output + "/IMG.MOV")
+                    
+                } catch {
+                    
+                }
+                JPEG(path: image).write(output + "/IMG.JPG",
+                                        assetIdentifier: assetIdentifier)
+                QuickTimeMov(path: mov).write(output + "/IMG.MOV",
+                                              assetIdentifier: assetIdentifier)
+                
+                //self?.livePhotoView.livePhoto = LPDLivePhoto.livePhotoWithImageURL(NSURL(fileURLWithPath: FilePaths.VidToLive.livePath.stringByAppendingString("/IMG.JPG")), videoURL: NSURL(fileURLWithPath: FilePaths.VidToLive.livePath.stringByAppendingString("/IMG.MOV")))
+                //self?.exportLivePhoto()
+                _ = DispatchQueue.main.sync {
+                    PHLivePhoto.request(withResourceFileURLs: [ URL(fileURLWithPath: FilePaths.VidToLive.livePath + "/IMG.MOV"), URL(fileURLWithPath: FilePaths.VidToLive.livePath + "/IMG.JPG")],
+                                        placeholderImage: nil,
+                                        targetSize: self!.view.bounds.size,
+                                        contentMode: PHImageContentMode.aspectFit,
+                                        resultHandler: { (livePhoto, info) -> Void in
+                                            self?.livePhotoView.livePhoto = livePhoto
+                                            
+                    })
+                }
+            }
+        }
+    }
+ 
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true) {
+            if let url = info[UIImagePickerControllerMediaURL] as? URL {
+                self.loadVideoWithVideoURL(url)
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func exportLivePhoto () {
+        PHPhotoLibrary.shared().performChanges({ () -> Void in
+            let creationRequest = PHAssetCreationRequest.forAsset()
+            let options = PHAssetResourceCreationOptions()
+            
+            creationRequest.addResource(with: PHAssetResourceType.pairedVideo, fileURL: URL(fileURLWithPath: FilePaths.VidToLive.livePath + "/IMG.MOV"), options: options)
+            creationRequest.addResource(with: PHAssetResourceType.photo, fileURL: URL(fileURLWithPath: FilePaths.VidToLive.livePath + "/IMG.JPG"), options: options)
+            
+        }, completionHandler: { (success, error) -> Void in
+            if !success {
+                DTLog((error?.localizedDescription)!)
+            }
+        })
     }
-    */
-
 }
